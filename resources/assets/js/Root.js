465a484abcc3e726bd537ec7800b1cfff496e48c
MargotDem <margot_demaulmont@hotmail.fr>
2018-04-22 22:41:39 +0200

    deleted react app to serve app through adonis, and set up the whole webpack workflow

---
+++ resources/assets/js/Root.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Root extends Component {
+  render () {
+    return (
+      <div>hey i am the root.</div>
+    )
+  }
+}
+
+export default Root
---
+++ resources/assets/js/app.js
@@ -0,0 +1,3 @@
+// console.log('VICTOIRE')
+// require('./bootstrap')
+require('./index')
