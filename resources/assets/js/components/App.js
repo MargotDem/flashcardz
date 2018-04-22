8eec53975b0c2e3ef7970c8286b9a9bf750dd77f
MargotDem <margot_demaulmont@hotmail.fr>
2018-04-22 23:30:45 +0200

    sass files

--- resources/assets/js/components/App.js
+++ resources/assets/js/components/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
     return (
       <CookiesProvider>
         <HashRouter>
-          <div>
+          <div className='App'>
             <Scroller />
             <Body />
           </div>
